6f1b8e24d6fab3e5f7d0a40d0118ba8a90bfcb14
Edswel <franckini@yahoo.com>
2022-05-10 11:30:59 +0100

    Basic styling for Expense Management

--- App.js
+++ App.js
@@ -50,9 +50,14 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: GlobalStyles.colors.header },
+          headerTintColor: 'white'
+        }}>
           <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} options={{ headerShown: false }} />
-          <Stack.Screen name='ManageExpense' component={ManageExpense} />
+          <Stack.Screen name='ManageExpense' component={ManageExpense} options={{
+            presentation: 'modal'
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
