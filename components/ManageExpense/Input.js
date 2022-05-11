2154b7952230be35f303bf92735ead8d4cad2834
Edswel <franckini@yahoo.com>
2022-05-11 12:46:27 +0100

    Add some syles to input field

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,12 +1,41 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 function Input({ label, TextInputConfig }) {
+    const inputStyles = [styles.input];
+
+    if (TextInputConfig && TextInputConfig.multiline) {
+        inputStyles.push(styles.inputMultiline)
+    }
+
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...TextInputConfig} />
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={inputStyles} {...TextInputConfig} />
         </View>
     );
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginHorizontal: 4,
+        marginVertical: 8
+    },
+    label: {
+        fontSize: 12,
+        color: 'black',
+        marginBottom: 4
+    },
+    input: {
+        backgroundColor: 'white',
+        padding: 4,
+        borderRadius: 6,
+        fontSize: 18,
+        color: 'black'
+    },
+    inputMultiline: {
+        minHeight: 100,
+        textAlignVertical: 'top'
+    }
+});
