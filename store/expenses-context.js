20a5aa14bc1796d410bb7cb51d70f5fdd1076bcc
Edswel <franckini@yahoo.com>
2022-05-13 10:21:54 +0100

    Using response data for unique id

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,45 +1,5 @@
 import { createContext, useReducer } from "react";
 
-// const DUMMY_EXPENSES = [
-//     {
-//         id: 'e1',
-//         description: 'A pair of shoes',
-//         amount: 99.99,
-//         date: new Date('2021-05-09')
-//     },
-//     {
-//         id: 'e2',
-//         description: 'Date Night',
-//         amount: 149.99,
-//         date: new Date('2022-04-19')
-//     },
-//     {
-//         id: 'e3',
-//         description: 'Some online course',
-//         amount: 199.99,
-//         date: new Date('2022-05-07')
-//     },
-//     {
-//         id: 'e4',
-//         description: 'Gin and Juice',
-//         amount: 9.99,
-//         date: new Date('2022-02-12')
-//     },
-//     {
-//         id: 'e5',
-//         description: 'Summer Vacation',
-//         amount: 1000.99,
-//         date: new Date('2021-08-09')
-//     },
-//     {
-//         id: 'd5',
-//         description: 'Upgrade Workspace',
-//         amount: 2500.99,
-//         date: new Date('2022-05-10')
-//     },
-// ];
-
-
 export const ExpensesContext = createContext({
     expenses: [],
     addExpense: ({ description, amount, date }) => { },
@@ -51,8 +11,8 @@ export const ExpensesContext = createContext({
 function expensesReducer(state, action) {
     switch (action.type) {
         case 'ADD':
-            const id = new Date().toString() + Math.random().toString();
-            return [{ ...action.payload, id: id }, ...state]
+            // const id = new Date().toString() + Math.random().toString();
+            return [action.payload, ...state]
         case 'SET':
             const inverted = action.payload.reverse();
             return inverted;
